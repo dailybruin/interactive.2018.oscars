42896aa69f1868408cad7408a7a932f89df6842b
kissyourmisery <21330xuchengyin@gmail.com>
2018-02-27 17:23:58 -0800

    added to gitignore

--- src/components/predictions/index.js
+++ src/components/predictions/index.js
@@ -1,38 +1,38 @@
 const BestPicKey = ["Call Me By Your Name", "Dunkirk", "Get Out", "Lady Bird", "Phantom Thread", "The Post", "The Shape of Water", "Three Billboards Outside Ebbing, Missouri"];
-const BestPicValue = [8, 3, 14, 17, 2, 3, 29, 13];
+const BestPicValue = [8.99, 3.37, 15.73, 19.1, 2.25, 3.37, 32.58, 14.61];
 
 const BestMaleLeadKey = ["Daniel Day-Lewis, “Phantom Thread”", "Daniel Kaluuya, “Get Out”", "Denzel Washington, “Roman J. Israel, Esq.”", "Gary Oldman, “Darkest Hour”", "Timothée Chalamet, “Call Me by Your Name”"];
-const BestMaleLeadValue = [13, 24, 5, 32, 16];
+const BestMaleLeadValue = [14.44, 26.67, 5.56, 35.56, 17.78];
 
 const BestFemaleLeadKey = ["Frances McDormand, “Three Billboards Outside Ebbing, Missouri”", "Margot Robbie, “I, Tonya”", "Meryl Streep, “The Post”", "Sally Hawkins, “The Shape of Water”", "Saoirse Ronan, “Lady Bird”"];
-const BestFemaleLeadValue = [27, 12, 9, 9, 32];
+const BestFemaleLeadValue = [30.34, 13.48, 10.11, 10.11, 35.96];
 
 const BestMaleSupportingKey = ["Christopher Plummer, “All the Money in the World”", "Richard Jenkins, “The Shape of Water”", "Sam Rockwell, “Three Billboards Outside Ebbing, Missouri”", "Willem Dafoe, “The Florida Project”", "Woody Harrelson, “Three Billboards Outside Ebbing, Missouri”"];
-const BestMaleSupportingValue = [8, 19, 30, 16, 17];
+const BestMaleSupportingValue = [30.0, 13.33, 10.0, 10.0, 35.56];
 
 const BestFemaleSupportingKey = ["Allison Janney, “I, Tonya”", "Laurie Metcalf, “Lady Bird”", "Lesley Manville, “Phantom Thread”", "Mary J. Blige, “Mudbound”", "Octavia Spencer, “The Shape of Water”"];
-const BestFemaleSupportingValue = [34, 22, 2, 7, 25];
+const BestFemaleSupportingValue = [37.78, 24.44, 2.22, 7.78, 27.78];
 
 const BestAnimationKey = ["“Coco,” Lee Unkrich, Darla K. Anderson", "“Ferdinand,” Carlos Saldanha", "“Loving Vincent,” Dorota Kobiela, Hugh Welchman, Sean Bobbitt, Ivan Mactaggart, Hugh Welchman", "“The Boss Baby,” Tom McGrath, Ramsey Ann Naito"];
-const BestAnimationValue = [84, 2, 3, 1];
+const BestAnimationValue = [93.33, 2.22, 3.33, 1.11];
 
 const BestDirectingKey = ["“Dunkirk,” Christopher Nolan", "“Get Out,” Jordan Peele", "“Lady Bird,” Greta Gerwig", "“Phantom Thread,” Paul Thomas Anderson", "“The Shape of Water,” Guillermo del Toro"];
-const BestDirectingValue = [21, 25, 14, 4, 26];
+const BestDirectingValue = [23.33, 27.78, 15.56, 4.44, 28.89];
 
 const BestForeignKey = ["“A Fantastic Woman” (Chile)", "“Loveless” (Russia)", "“On Body and Soul (Hungary)", "“The Insult” (Lebanon)", "“The Square” (Sweden)"];
-const BestForeignValue = [40, 11, 8, 10, 20];
+const BestForeignValue = [44.44, 12.22, 8.89, 11.11, 22.22];
 
 const BestScoreKey = ["“Dunkirk,” Hans Zimmer", "“Phantom Thread,” Jonny Greenwood", "“Star Wars: The Last Jedi,” John Williams", "“The Shape of Water,” Alexandre Desplat", "“Three Billboards Outside Ebbing, Missouri,” Carter Burwell"];
-const BestScoreValue = [39, 3, 19, 26, 3];
+const BestScoreValue = [43.33, 3.33, 21.11, 28.89, 3.33];
 
 const BestSongKey = ["“Mighty River” from “Mudbound,” Mary J. Blige", "“Mystery of Love” from “Call Me by Your Name,” Sufjan Stevens", "“Remember Me” from “Coco,” Kristen Anderson-Lopez, Robert Lopez", "“Stand Up for Something” from “Marshall,” Diane Warren, Common", "“This Is Me” from “The Greatest Showman,” Benj Pasek, Justin Paul"];
-const BestSongValue = [4, 28, 32, 1, 25];
+const BestSongValue = [4.44, 31.11, 35.56, 1.11, 27.78];
 
 const BestOriginalKey = ["“Get Out,” Jordan Peele", "“Lady Bird,” Greta Gerwig", "“The Big Sick,” Emily V. Gordon & Kumail Nanjiani", "“The Shape of Water,” Guillermo del Toro, Vanessa Taylor", "“Three Billboards Outside Ebbing, Missouri,” Martin McDonagh"];
-const BestOriginalValue = [36, 25, 13, 8, 8];
+const BestOriginalValue = [40.0, 27.78, 14.44, 8.89, 8.89];
 
 const BestAdaptedKey = ["“Call Me by Your Name,” James Ivory", "“Logan,” Scott Frank & James Mangold and Michael Green", "“Molly’s Game,” Aaron Sorkin", "“Mudbound,” Virgil Williams and Dee Rees", "“The Disaster Artist,” Scott Neustadter & Michael H. Weber"];
-const BestAdaptedValue = [53, 5, 8, 2, 22];
+const BestAdaptedValue = [58.89, 5.56, 8.89, 2.22, 24.44];
 
 const EightColors = ["#ffdd40", "#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850", "#000", "#ff69b4"];
 
