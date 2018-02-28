a582b0ba7f1cbd780486d20a7ca7dddbfd16047f
Nathan Smith <nathan@nathansmith.io>
2018-02-28 10:57:16 -0800

    resize no jquery

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,1 +1,10 @@
-$('.cover-photo').height($(window).height());
+function resize() {
+  document.getElementById('cover-photo').style.height = `${
+    window.innerHeight
+  }px`;
+}
+
+resize();
+window.onresize = () => {
+  resize();
+};
